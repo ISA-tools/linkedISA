5a1463ebd908fb1ae76eef1e3e68384e6b5d7d3b
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-10-22 23:35:53 +0100

    Creating individuals for study and some of its properties and printing ontology

--- src/main/java/org/isatools/isa2owl/ISAtab2OWLConverter.java
+++ src/main/java/org/isatools/isa2owl/ISAtab2OWLConverter.java
@@ -1,15 +1,17 @@
 package org.isatools.isa2owl;
 
 import org.apache.log4j.Logger;
+import org.coode.owlapi.manchesterowlsyntax.ManchesterOWLSyntaxOntologyFormat;
 import org.isatools.isacreator.io.importisa.ISAtabFilesImporter;
 import org.isatools.isacreator.io.importisa.ISAtabImporter;
 import org.isatools.isacreator.model.*;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.io.OWLOntologyDocumentTarget;
+import org.semanticweb.owlapi.io.OWLXMLOntologyFormat;
+import org.semanticweb.owlapi.io.SystemOutDocumentTarget;
+import org.semanticweb.owlapi.model.*;
 
+import java.io.File;
 import java.util.Map;
 
 
@@ -38,15 +40,41 @@ public class ISAtab2OWLConverter {
 	 * 
 	 * @param cDir directory where the ISA configuration file can be found
 	 */
-	public ISAtab2OWLConverter(String cDir, ISASyntax2OWLMapping mapping){
+	public ISAtab2OWLConverter(String cDir, ISASyntax2OWLMapping m){
 		configDir = cDir;
         log.debug("configDir="+configDir);
+        mapping = m;
 		importer = new ISAtabFilesImporter(configDir);
 		System.out.println("importer="+importer);
         manager = OWLManager.createOWLOntologyManager();
         factory = manager.getOWLDataFactory();
+        try{
+        ontology = manager.createOntology(ontoIRI);
+        }catch(OWLOntologyCreationException e){
+            e.printStackTrace();
+        }
+
 
 	}
+
+    private void processSourceOntologies(){
+        Map<String,IRI> sourceOntoIRIs = mapping.getSourceOntoIRIs();
+        OWLOntology onto = null;
+
+        //TODO check imports from ontologies where the import chain implies that ontologies are duplicated
+        for(IRI iri: sourceOntoIRIs.values()){
+            try{
+                System.out.println("iri="+iri);
+                onto = manager.loadOntology(iri);
+                OWLImportsDeclaration importDecl = factory.getOWLImportsDeclaration(iri);
+                manager.applyChange(new AddImport(ontology, importDecl));
+
+            }catch(OWLOntologyCreationException oocrex){
+                //oocrex.printStackTrace();
+            }
+        }
+
+    }
 	
 	
 	private boolean readInISAFiles(String parentDir){
@@ -63,6 +91,9 @@ public class ISAtab2OWLConverter {
 		if (!readInISAFiles(parentDir)){
             System.out.println(importer.getMessagesAsString());
         }
+
+        processSourceOntologies();
+
 		Investigation investigation = importer.getInvestigation();
         System.out.println("investigation=" + investigation);
         log.debug("investigation=" + investigation);
@@ -71,6 +102,25 @@ public class ISAtab2OWLConverter {
 		for(String key: studies.keySet()){
 			populateStudy(studies.get(key));
 		}
+
+        try{
+        File file = new File("/Users/agbeltran/workspace-private/isa2owl/isatab-example.owl");
+        manager.saveOntology(ontology, IRI.create(file.toURI()));
+
+        // We can also dump an ontology to System.out by specifying a different OWLOntologyOutputTarget
+        // Note that we can write an ontology to a stream in a similar way using the StreamOutputTarget class
+        OWLOntologyDocumentTarget documentTarget = new SystemOutDocumentTarget();
+        // Try another format - The Manchester OWL Syntax
+        ManchesterOWLSyntaxOntologyFormat manSyntaxFormat = new ManchesterOWLSyntaxOntologyFormat();
+        OWLXMLOntologyFormat format = new OWLXMLOntologyFormat();
+        if(format.isPrefixOWLOntologyFormat()) {
+            manSyntaxFormat.copyPrefixesFrom(format.asPrefixOWLOntologyFormat());
+        }
+        //save ontology
+        manager.saveOntology(ontology, manSyntaxFormat, new SystemOutDocumentTarget());
+        }catch(OWLOntologyStorageException e){
+        e.printStackTrace();
+        }
 
         return true;
 	}
@@ -81,8 +131,37 @@ public class ISAtab2OWLConverter {
 
 
         //Study
-        IRI type = mapping.getTypeMapping("Study");
-        factory.getOWLNamedIndividual(ontoIRI.create(study.getStudyId()));
+        IRI study_class_iri = mapping.getTypeMapping("Study");
+        OWLNamedIndividual study_individual = factory.getOWLNamedIndividual(ontoIRI.create(study.getStudyId()));
+        OWLClass study_class = factory.getOWLClass(study_class_iri);
+        OWLClassAssertionAxiom study_class_assertion = factory.getOWLClassAssertionAxiom(study_class, study_individual);
+        manager.addAxiom(ontology,study_class_assertion);
+
+        //Study identifier
+        IRI study_identifier_class_iri = mapping.getTypeMapping(Study.STUDY_ID);
+        OWLNamedIndividual study_identifier_individual = factory.getOWLNamedIndividual(ontoIRI.create(study.getStudyId()+"_identifier"));
+        OWLClass study_identifier_class = factory.getOWLClass(study_identifier_class_iri);
+        OWLClassAssertionAxiom study_identifier_class_assertion = factory.getOWLClassAssertionAxiom(study_identifier_class, study_identifier_individual);
+        manager.addAxiom(ontology,study_identifier_class_assertion);
+
+        //properties for Study identifier
+        //TODO
+
+        //Study title
+        IRI study_title_iri = mapping.getTypeMapping(Study.STUDY_TITLE);
+        OWLNamedIndividual study_title_individual = factory.getOWLNamedIndividual(ontoIRI.create(study.getStudyId()+"_title"));
+        OWLClass study_title_class = factory.getOWLClass(study_title_iri);
+        OWLClassAssertionAxiom study_title_class_assertion = factory.getOWLClassAssertionAxiom(study_title_class, study_title_individual);
+        manager.addAxiom(ontology,study_title_class_assertion);
+
+        //TODO add properties for title
+
+        //Study description
+        IRI study_description_iri = mapping.getTypeMapping(Study.STUDY_DESC);
+        OWLNamedIndividual study_description_individual = factory.getOWLNamedIndividual(ontoIRI.create(study.getStudyId()+"_description"));
+        OWLClass study_description_class = factory.getOWLClass(study_description_iri);
+        OWLClassAssertionAxiom study_description_class_assertion = factory.getOWLClassAssertionAxiom(study_description_class, study_description_individual);
+        manager.addAxiom(ontology,study_description_class_assertion);
 
 
 
