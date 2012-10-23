91581ac54841ee1dc5a9b4c52aef33b0779f770d
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-10-23 16:39:14 +0100

    Class for the extended ISA syntax

---
+++ src/main/java/org/isatools/isa2owl/ExtendedISASyntax.java
@@ -0,0 +1,16 @@
+package org.isatools.isa2owl;
+
+/**
+ * Created by the ISATeam.
+ * User: agbeltran
+ * Date: 23/10/2012
+ * Time: 11:23
+ *
+ * @author <a href="mailto:alejandra.gonzalez.beltran@gmail.com">Alejandra Gonzalez-Beltran</a>
+ */
+public class ExtendedISASyntax {
+
+   public static String STUDY = "Study";
+   public static String PUBLICATION = "Publication";
+
+}
