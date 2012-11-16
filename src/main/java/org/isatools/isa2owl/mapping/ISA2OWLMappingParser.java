0e9a6d202160c42def75bfc76b48cd07bb62a9d5
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-11-16 09:57:32 +0100

    isa2owl code

--- src/main/java/org/isatools/isa2owl/mapping/ISA2OWLMappingParser.java
+++ src/main/java/org/isatools/isa2owl/mapping/ISA2OWLMappingParser.java
@@ -1,15 +1,14 @@
  
-package org.isatools.isa2owl;
+package org.isatools.isa2owl.mapping;
+
+import au.com.bytecode.opencsv.CSVReader;
+import org.apache.log4j.Logger;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import au.com.bytecode.opencsv.CSVReader;
-
 //import org.isatools.isacreator.model.*;
-
-import org.semanticweb.owlapi.model.IRI;
 
 /**
  * Mapping from ISA format to OWL.
@@ -18,9 +17,11 @@ import org.semanticweb.owlapi.model.IRI;
  *
  */
 public class ISA2OWLMappingParser {
+
+    private static final Logger log = Logger.getLogger(ISA2OWLMappingParser.class);
 	
 	
-	private ISA2OWLMapping mapping = null;
+	private ISASyntax2OWLMapping mapping = null;
 		
 	private static enum MappingFileField {
 		ONTOLOGIES,
@@ -49,11 +50,11 @@ public class ISA2OWLMappingParser {
 	
 	
 	public ISA2OWLMappingParser(){
-		mapping = new ISA2OWLMapping();
+		mapping = new ISASyntax2OWLMapping();
 	}
 	
 	
-	public ISA2OWLMapping getMapping(){
+	public ISASyntax2OWLMapping getMapping(){
 		return mapping;
 	}
 	
@@ -67,11 +68,11 @@ public class ISA2OWLMappingParser {
 	    MappingFileField currentField = null;
 	    
 	    	while ((nextLine = csvReader.readNext()) != null) {
-	    		
+	    		//System.out.println("nextLine="+nextLine[0]);
+
 	    		if (nextLine!=null && MappingFileField.isMappingField(nextLine[0])){
 	    			currentField = MappingFileField.getField(nextLine[0]);
 	    			System.out.println("currentField="+currentField);
-	    			
 	    			
 	    		}else{
 	    			if (nextLine[0].equals(""))
@@ -89,6 +90,9 @@ public class ISA2OWLMappingParser {
 	    			case PATTERNS:	
 	    				parsePatterns(nextLine);
 	    				break;
+                    default:
+                        parsePatterns(nextLine);
+                        break;
 	    			}
 	    			
 	    		}
@@ -109,6 +113,7 @@ public class ISA2OWLMappingParser {
 	private void parseOntology(String[] line){
 		//ONTOLOGY LINE FORMAT
 		//name	version	IRI
+        log.debug("*********adding ontology " + line[0] + "," + line[2]);
 		mapping.addOntology(line[0],line[2]);
 		
 	}
@@ -116,26 +121,56 @@ public class ISA2OWLMappingParser {
 	private void parseAuthor(String[] line){
 		
 	}
-	
-	private void parseMapping(String[] line){
 
-        //MAPPING LINE FORMAT
-        //label; definition; uri of equiv class; uri of superclass; string; uri of property; string; uri of object; string; ...
-		mapping.addDefMapping(line[0], line[1]);
-		mapping.addEquivClassMapping(line[0], line[2]);
-		mapping.addSubClassMapping(line[0], line[3]);
-		
-		int i = 5;
-		while(i<(line.length-2) && !line[i].equals("") && !line[i+2].equals("")){
-			mapping.addPropertyMapping(line[0], line[i], line[i+2]);
-			System.out.println("Property mapping="+line[i]+","+line[i+2]);
-			i = i+4;
-		} 
-		
-	}
+    private void parseMapping(String[] line){
+
+        if (!line[0].startsWith("{")){
+
+            mapping.addTypeMapping(line[0], line[2]);
+
+            //parsing property mappings
+            int i = 4;
+            while(i<(line.length-2) && !line[i].equals("") && !line[i+2].equals("")){
+                mapping.addPropertyMapping(line[0], line[i], line[i+2]);
+                i = i+4;
+            }
+
+        }else{
+
+            String element = line[0];
+            element = element.substring(1,element.length()-1);
+            String[] types = element.split("\\|");
+
+            for(int i=0; i<types.length; i++){
+                mapping.addTypeMapping(types[i],line[2]);
+            }
+
+            //parsing property mappings
+            int i = 4;
+            while(i<(line.length-2) && !line[i].equals("") && !line[i+2].equals("")){
+
+                String object = line[i+2];
+
+                if (!object.startsWith("{")){
+
+                    for(int j=0; j<types.length; j++)
+                        mapping.addPropertyMapping(types[j], line[i],object);
+
+                }else{
+                    object = object.substring(1, object.length()-1);
+                    String[] objects = object.split(ISASyntax2OWLMapping.SEPARATOR_REGEXPR);
+
+                    for(int j=0; j<types.length; j++){
+                        mapping.addPropertyMapping(types[j], line[i], objects[j]);
+                    }
+                }
+                i = i+4;
+            }
+        }
+    }
 	
 	private void parsePatterns(String[] line){
-		
+
 	}
 	
 }
