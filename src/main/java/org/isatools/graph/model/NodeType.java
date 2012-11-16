9e7b934a5e10d2d2ea9a86eb292803ba08af6cb9
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-11-16 09:47:29 +0100

    Graph analysis code

---
+++ src/main/java/org/isatools/graph/model/NodeType.java
@@ -0,0 +1,15 @@
+package org.isatools.graph.model;
+
+/**
+ * Created by the ISA team
+ *
+ * @author Eamonn Maguire (eamonnmag@gmail.com)
+ *         <p/>
+ *         Date: 11/10/2011
+ *         Time: 11:46
+ */
+public enum NodeType {
+
+    DATA_NODE, MATERIAL_NODE, PROCESS_NODE
+
+}
