f46dfd2400a7639765ca9f2094c61a3b64a1036d
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2013-04-25 23:10:15 +0100

    Changes to import statements

--- isa2owl-core/src/main/java/org/isatools/owl/ReasonerService.java
+++ isa2owl-core/src/main/java/org/isatools/owl/ReasonerService.java
@@ -1,14 +1,13 @@
 package org.isatools.owl;
 
 
+import com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory;
 import org.apache.log4j.Logger;
-
-import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
-import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
-
-import com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory;
 
 /**
  * Created by the ISATeam.
