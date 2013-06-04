10147285f4d2606c82d2e09add58dd410c8992c0
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2013-06-04 04:50:08 +0100

    Mapping files

---
+++ isa2owl-core/src/main/java/org/isatools/isa2owl/mapping/ISASyntax2OWLMappingFiles.java
@@ -0,0 +1,16 @@
+package org.isatools.isa2owl.mapping;
+
+/**
+ * Created by the ISATeam.
+ * User: agbeltran
+ * Date: 04/06/2013
+ * Time: 04:48
+ *
+ * @author <a href="mailto:alejandra.gonzalez.beltran@gmail.com">Alejandra Gonzalez-Beltran</a>
+ */
+public class ISASyntax2OWLMappingFiles {
+
+    public static final String ISA_OBI_MAPPING_FILENAME = "mappings/ISA-OBI-mapping.csv";
+    public static final String ISA_SIO_MAPPING_FILENAME = "mappings/ISA-SIO-mapping.csv";
+
+}
