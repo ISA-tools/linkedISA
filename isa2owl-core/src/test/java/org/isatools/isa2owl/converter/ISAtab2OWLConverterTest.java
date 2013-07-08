028a4c1577e42a9f2c35b456bf7bee41c4cc62b6
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2013-07-09 00:04:31 +0100

    Adding ISA ontology mapping.

--- isa2owl-core/src/test/java/org/isatools/isa2owl/converter/ISAtab2OWLConverterTest.java
+++ isa2owl-core/src/test/java/org/isatools/isa2owl/converter/ISAtab2OWLConverterTest.java
@@ -37,10 +37,12 @@ public class ISAtab2OWLConverterTest {
 
         parser = new ISA2OWLMappingParser();
         URL isa_obi_mapping_url = getClass().getClassLoader().getResource(ISASyntax2OWLMappingFiles.ISA_OBI_MAPPING_FILENAME);
+        System.out.println("isa_obi_mapping_url="+isa_obi_mapping_url);
         parser.parseCSVMappingFile(isa_obi_mapping_url.toURI().getRawPath().toString());
 
         URL isa_isa_mapping_url = getClass().getClassLoader().getResource(ISASyntax2OWLMappingFiles.ISA_ISA_MAPPING_FILENAME);
-        parser.parseCSVMappingFile(isa_obi_mapping_url.toURI().getRawPath().toString());
+        System.out.println("isa_isa_mapping_url="+isa_isa_mapping_url);
+        parser.parseCSVMappingFile(isa_isa_mapping_url.toURI().getRawPath().toString());
 
         mapping = parser.getMapping();
 
