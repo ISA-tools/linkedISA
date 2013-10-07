de2e822b0adb1227feaf7bc57c264b09720d8182
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2013-10-07 11:23:16 +0100

    BFO terms

--- isa2owl-core/src/main/java/org/isatools/owl/BFO.java
+++ isa2owl-core/src/main/java/org/isatools/owl/BFO.java
@@ -13,21 +13,18 @@ package org.isatools.owl;
 public class BFO {
 
     public static final String NAMESPACE = "http://purl.obolibrary.org/obo/BFO_";
-    public static final String SHORTNAME = "OBI";
+    public static final String SHORTNAME = "BFO";
 
-
-//    public static final IRI BFO_INDEPENDENT_CONTINUANT_IRI = IRI.create("http://purl.obolibrary.org/obo/BFO_0000004");
-//
-//    public static final IRI BFO_REALIZES = IRI.create("http://purl.obolibrary.org/obo/BFO_0000055");
-//    public static final IRI BFO_CONCRETIZES = IRI.create("http://purl.obolibrary.org/obo/BFO_0000059");
-
-//public static final IRI BFO_IS_PART_OF = IRI.create("http://purl.obolibrary.org/obo/BFO_0000050");
-
-
+    public static final String QUALITY = NAMESPACE + "0000019";
     public static final String HAS_QUALITY = NAMESPACE + "0000086";
+
     public static final String IS_PART_OF = NAMESPACE + "0000050";
     public static final String MATERIAL_ENTITY = NAMESPACE + "0000040";
+
+    public static final String REALIZES = NAMESPACE + "0000055";
+    public static final String CONCRETIZES = NAMESPACE + "0000059";
     public static final String IS_BEARER_OF = NAMESPACE + "0000053";
+    public static final String INDEPENDENT_CONTINUANT = NAMESPACE + "0000004";
     public static final String DEPENDENT_CONTINUANT = NAMESPACE + "0000005";
 
 
