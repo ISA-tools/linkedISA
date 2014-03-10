f15bec1bebfc5ea6aedac28dd0964d9b51b41e88
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2014-03-10 18:02:38 +0000

    added test

--- isa2owl-core/src/test/java/org/isatools/isa2owl/converter/ISAtab2OWLConverterTest.java
+++ isa2owl-core/src/test/java/org/isatools/isa2owl/converter/ISAtab2OWLConverterTest.java
@@ -234,4 +234,20 @@ public class ISAtab2OWLConverterTest {
     }
 
 
+    @Test
+    public void testConvertISATABTest1(){
+        iri = "http://w3id.org/isa/isatab-test1";
+        isatabParentDir = getClass().getResource( path + "isatab-test1").getFile();
+        System.out.println("isatabParentDir="+isatabParentDir);
+        System.out.println("Converting the ISA-tab dataset into OWL");
+        isatab2owl.convert(isatabParentDir, iri);
+        isatab2owl.saveOntology(savePath+"isatab-test1.rdf");
+
+        System.out.println();
+        System.out.println();
+        isatab2owl.getMintedIRIs();
+
+    }
+
+
 }
