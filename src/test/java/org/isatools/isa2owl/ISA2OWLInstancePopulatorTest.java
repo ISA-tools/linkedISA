b7415b5653ef2304e382ff7f541e59fdd92e1be8
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2014-04-08 12:10:36 +0100

    Changes

---
+++ src/test/java/org/isatools/isa2owl/ISA2OWLInstancePopulatorTest.java
@@ -0,0 +1,45 @@
+package org.isatools.isa2owl;
+
+import java.net.URL;
+
+import org.isatools.isacreator.io.importisa.ISAtabImporter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Test class for ISA2OWLInstancePopulator
+ * 
+ * @author <a href="mailto:alejandra.gonzalez.beltran@gmail.com">Alejandra Gonzalez-Beltran</a>
+ *
+ */
+public class ISA2OWLInstancePopulatorTest {
+
+	private ISA2OWLInstancePopulator populator = null;
+	private String configDir = null;
+	private String isatabParentDir = null;
+	
+	@Before
+    public void setUp() throws Exception {
+		
+		String baseDir = System.getProperty("basedir");
+		System.out.println("baseDir="+baseDir);
+    	configDir = baseDir + "/src/test/resources/configurations/isaconfig-default_v2011-02-18/";
+    	System.out.println("configDir="+configDir);
+        isatabParentDir = baseDir + "/src/test/resources/BII-I-1-URI";
+        System.out.println("isatabParentDir="+isatabParentDir);
+		populator = new ISA2OWLInstancePopulator(configDir);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testPopulate() {
+        assert(populator.populateOntology(isatabParentDir));
+
+    }
+	
+}
